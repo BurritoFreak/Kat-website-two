8f6a353868a8d69a5acf47dd88b19ef6f1782269
BurritoFreak <chris.sarmousakis@gmail.com>
2024-09-03 02:24:19 -0400

    Updated names

--- content/links.js
+++ content/links.js
@@ -1,37 +1,37 @@
 //React element in json form with all of my links
 
 const myLinks = [{
-    "name": "OnlyFans",
+    "name": "🌶️OnlyFans🌶️",
     "url": "https://onlyfans.com/emokitten_xoxo/c17",
     "icon": "fa-solid fa-pepper-hot",
     "image": "OnlyFans.jpg",
     "followbar": true
 },{
-    "name": "Tik Tok",
+    "name": "🌸Tik Tok🌸",
     "url": "https://www.tiktok.com/@emokitten_xoxo",
     "icon": "fab fa-tiktok",
     "image": "TikTok.jpg",
     "followbar": true
 },{
-    "name": "Instagram",
+    "name": "💜Instagram💜",
     "url": "https://www.instagram.com/emokitten_xoxo/",
     "icon": "fab fa-instagram",
     "image": "Instagram.jpg",
     "followbar": true
 },{
-    "name": "Twitter",
+    "name": "✨Twitter✨",
     "url": "https://twitter.com/emokitten_xoxo",
     "icon": "fab fa-twitter",
     "image": "Twitter.jpg",
     "followbar": true
 },{
-    "name": "Discord",
+    "name": "🎤Discord🎤",
     "url": "https://discord.gg/bBgkxS266t",
     "icon": "fab fa-discord",
     "image": "Discord.jpg",
     "followbar": true
 },{
-    "name": "Twitch",
+    "name": "📸Twitch📸",
     "url": "https://www.twitch.tv/emokitten_xoxo",
     "icon": "fab fa-twitch",
     "image": "Twitch.jpg",
