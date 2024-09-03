a97c72c433a3b6e50b83c59278df4cf897750a08
BurritoFreak <chris.sarmousakis@gmail.com>
2024-09-02 20:08:36 -0400

    Added Links with pictures

---
+++ content/links.js
@@ -0,0 +1,36 @@
+
+//React element in json form with all of my links
+
+const myLinks = [{
+    "name": "OnlyFans",
+    "url": "https://onlyfans.com/emokitten_xoxo/c17",
+    "icon": "fa-solid fa-pepper-hot",
+    "image": "OnlyFans.jpg"
+},{
+    "name": "Tik Tok",
+    "url": "https://www.tiktok.com/@emokitten_xoxo",
+    "icon": "fab fa-tiktok",
+    "image": "TikTok.jpg"
+},{
+    "name": "Instagram",
+    "url": "https://www.instagram.com/emokitten_xoxo/",
+    "icon": "fab fa-instagram",
+    "image": "Instagram.jpg"
+},{
+    "name": "Twitter",
+    "url": "https://twitter.com/emokitten_xoxo",
+    "icon": "fab fa-twitter",
+    "image": "Twitter.jpg"
+},{
+    "name": "Discord",
+    "url": "https://discord.gg/bBgkxS266t",
+    "icon": "fab fa-discord",
+    "image": "Discord.jpg"
+},{
+    "name": "Twitch",
+    "url": "https://www.twitch.tv/emokitten_xoxo",
+    "icon": "fab fa-twitch",
+    "image": "Twitch.jpg"
+}];
+
+export default myLinks
